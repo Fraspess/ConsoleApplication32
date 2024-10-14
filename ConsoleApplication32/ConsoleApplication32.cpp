886f610b09b0555b226913b30a0ace447477da2b
Frasp <pavlosamchyk@gmail.com>
2024-10-14 16:23:10 +0300

    start

--- ConsoleApplication32/ConsoleApplication32.cpp
+++ ConsoleApplication32/ConsoleApplication32.cpp
@@ -9,6 +9,17 @@ int main()
     std::cout << "Hello World!\n";
     std::cout << "Hello World!\n";
     std::cout << "Hello World!\n";
+    std::cout << "Hello World!\n";
+    std::cout << "Hello World!\n";
+    std::cout << "Hello World!\n";
+    std::cout << "Hello World!\n";
+    std::cout << "Hello World!\n";
+    std::cout << "Hello World!\n";
+    std::cout << "Hello World!\n";
+    std::cout << "Hello World!\n";
+    std::cout << "Hello World!\n";
+    std::cout << "Hello World!\n";
+    std::cout << "Hello World!\n";
 }
 
 
